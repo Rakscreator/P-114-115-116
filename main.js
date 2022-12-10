3ce6d5339e40928c2c6e26116cb1ea7f2c658898
Rakscreator <90820664+Rakscreator@users.noreply.github.com>
2022-12-10 22:40:08 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function preload(){
+
+}
+function setup(){
+    canvas = createCanvas(640,480)
+    canvas.center()
+    video = createCapture(VIDEO)
+    video.size(640,480)
+    video.hide()
+}
+function draw(){
+}
+function takeSnapshot(){
+
+}
