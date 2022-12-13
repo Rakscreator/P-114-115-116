1d80fb825cb2d0ef2477c8fec44626de53478694
Rakscreator <90820664+Rakscreator@users.noreply.github.com>
2022-12-13 19:13:00 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -7,9 +7,23 @@ function setup(){
     video = createCapture(VIDEO)
     video.size(640,480)
     video.hide()
+
+    poseNet = ml5.poseNet(video,modelloaded)
+    poseNet.on('pose',gotPoses)
 }
 function draw(){
+    image(video,0,0,640,480)
 }
 function takeSnapshot(){
-
+    save('pic-with-sunglasses')
+}
+function modelloaded(){
+    console.log('posenet is activated')
+}
+function gotPoses(results){
+    if(results.length > 0){
+        console.log(results)
+        console.log("right eye x = " + results[0].pose.rightEye.x)
+        console.log("right eye y = " + results[0].pose.rightEye.y)
+    }
 }
