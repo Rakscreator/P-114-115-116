03906a7971b38da5cb572485ebc28883fdaaa969
Rakscreator <90820664+Rakscreator@users.noreply.github.com>
2022-12-19 09:23:58 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
+rightEyex = 0
+rightEyey = 0
 function preload(){
-
+    img = loadImage("sunglasses IMG.png")
 }
 function setup(){
     canvas = createCanvas(640,480)
@@ -13,6 +15,7 @@ function setup(){
 }
 function draw(){
     image(video,0,0,640,480)
+    image(img,rightEyex-55,rightEyey-90,200,200)
 }
 function takeSnapshot(){
     save('pic-with-sunglasses')
@@ -23,6 +26,8 @@ function modelloaded(){
 function gotPoses(results){
     if(results.length > 0){
         console.log(results)
+        rightEyex = results[0].pose.rightEye.x
+        rightEyey = results[0].pose.rightEye.y
         console.log("right eye x = " + results[0].pose.rightEye.x)
         console.log("right eye y = " + results[0].pose.rightEye.y)
     }
